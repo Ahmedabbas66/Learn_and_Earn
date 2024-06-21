c9a889f4fc44469f6d2036f37783e2c5308df3d8
mav7777 <mohamed.AYMAN70418@ci.menofia.edu.eg>
2024-06-21 05:50:47 +0300

    v9

--- admin/exams_list.php
+++ admin/exams_list.php
@@ -4,12 +4,12 @@
 // Include the database connection file
 include '../components/connect.php';
 
-if(isset($_COOKIE['tutor_id'])){
+if (isset($_COOKIE['tutor_id'])) {
     $tutor_id = $_COOKIE['tutor_id'];
- }else{
+} else {
     $tutor_id = '';
     header('location:login.php');
- }
+}
 
 // Fetch all exams from the database
 $select_exams = $conn->prepare("SELECT * FROM exams");
@@ -19,38 +19,41 @@ $exams = $select_exams->fetchAll(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Exam List</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exam List</title>
 
-<!-- font awesome cdn link  -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-<!-- custom css file link  -->
-<link rel="stylesheet" href="../css/admin_style.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/admin_style.css">
 
 </head>
+
 <body>
 
-<?php include '../components/admin_header.php'; ?>
+    <?php include '../components/admin_header.php'; ?>
 
-<section class="exam-list">
-    <h1 class="heading">Exam List</h1>
+    <section class="exam-list">
+        <h1 class="heading">Exam List</h1>
 
-    <ul>
-        <?php foreach ($exams as $exam) : ?>
-            <li>
-                <a href="display_exam.php?exam_id=<?= $exam['id'] ?>"><?= htmlspecialchars($exam['title']) ?></a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</section>
+        <ul>
+            <?php foreach ($exams as $exam) : ?>
+                <li>
+                    <h1 class="heading"><a href="display_exam.php?exam_id=<?= $exam['id'] ?>"><?= htmlspecialchars($exam['title']) ?></a></h1>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
 
-<?php include '../components/footer.php'; ?>
+    <?php include '../components/footer.php'; ?>
 
-<script src="../js/admin_script.js"></script>
+    <script src="../js/admin_script.js"></script>
 
 
 </body>
+
 </html>
